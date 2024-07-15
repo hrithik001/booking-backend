761c27132a443a940a25cfa7b6ae043cded32707
itsmehrithikroshan@gmail.com <itsmehrithikroshan@gmail.com>
2024-07-15 15:12:14 +0530

    updated uploading route and implemented new routes for place regarding get update post

--- src/routes/place.routes.js
+++ src/routes/place.routes.js
@@ -1,15 +1,17 @@
 import express from 'express';
 import {uploadByLink} from '../controllers/upload.controller.js'
-import { addNewPlace } from '../controllers/place.controller.js';
+import { addNewPlace ,getAllPlaces ,getSinglePlace, updatePlaceData} from '../controllers/place.controller.js';
 
 
 const placeRoutes = express.Router();
 
 
 
-
+placeRoutes.get('/',getAllPlaces);
+placeRoutes.get('/:id',getSinglePlace);
 placeRoutes.post('/upload/by-link',uploadByLink);
-placeRoutes.post('/upload/new',addNewPlace);
+placeRoutes.post('/',addNewPlace);
+placeRoutes.put('/:id',updatePlaceData);
 // placeRoutes.post('/upload/by-file',photoUploadMiddleware,uploadByFile);
 
 export default placeRoutes;
