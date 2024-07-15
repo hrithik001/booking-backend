2ff4f4828868ef07dbcbea69aaf9d66c7d43711a
itsmehrithikroshan@gmail.com <itsmehrithikroshan@gmail.com>
2024-07-15 22:06:28 +0530

    added route to fetch places added by user

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { registerUser,loginUser, userProfile ,userLogout} from '../controllers/user.controller.js';
+import { getUserPlaces } from '../controllers/place.controller.js';
 
 const userRoutes = express.Router();
 
@@ -7,5 +8,7 @@ userRoutes.post('/register', registerUser);
 userRoutes.post('/login',loginUser)
 userRoutes.get('/profile',userProfile);
 userRoutes.post('/logout',userLogout);
+userRoutes.get('/places',getUserPlaces)
+
 
 export default userRoutes;
